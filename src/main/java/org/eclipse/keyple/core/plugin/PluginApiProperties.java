5c70a0589b229e8cea30afdbd3fc59d9ab07b367
Andrei Cristea <andrei.cristea019@gmail.com>
2023-11-08 14:32:14 +0100

    wip

--- src/main/java/org/eclipse/keyple/core/plugin/PluginApiProperties.java
+++ src/main/java/org/eclipse/keyple/core/plugin/PluginApiProperties.java
@@ -22,7 +22,7 @@ public final class PluginApiProperties {
    *
    * @since 2.0.0
    */
-  public static final String VERSION = "2.1";
+  public static final String VERSION = "2.2";
 
   /** Private constructor */
   private PluginApiProperties() {}
