c963a8c18c4deaa630762817a8debd3f19c1df04
andrei-cristea <andrei.cristea019@gmail.com>
2024-03-27 16:53:46 +0100

    feat: restore processing state (#12)

---
+++ src/main/java/org/eclipse/keyple/core/plugin/spi/reader/observable/state/processing/CardPresenceMonitorBlockingSpi.java
@@ -0,0 +1,46 @@
+/* **************************************************************************************
+ * Copyright (c) 2019 Calypso Networks Association https://calypsonet.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ************************************************************************************** */
+package org.eclipse.keyple.core.plugin.spi.reader.observable.state.processing;
+
+import org.eclipse.keyple.core.plugin.ReaderIOException;
+import org.eclipse.keyple.core.plugin.TaskCanceledException;
+
+/**
+ * Reader able to detect and notify a card removal during processing, between two APDU commands.
+ *
+ * <p>A typical example of readers conforming to this mode of operation are PC/SC type readers.
+ *
+ * @since 2.3.0
+ */
+public interface CardPresenceMonitorBlockingSpi {
+
+  /**
+   * Monitors the card presence indefinitely (the method is blocking as long as the card is
+   * present).
+   *
+   * <p>This monitoring can be cancelled for an internal (for example timeout) or external reason
+   * (for example invocation of {@link #stopCardPresenceMonitoringDuringProcessing()}), in this case
+   * an exception is raised.
+   *
+   * @throws ReaderIOException If the communication with the reader has failed.
+   * @throws TaskCanceledException If the task has been canceled and is no longer active.
+   * @since 2.3.0
+   */
+  void monitorCardPresenceDuringProcessing() throws ReaderIOException, TaskCanceledException;
+
+  /**
+   * Interrupts the monitoring of the card presence.
+   *
+   * @since 2.3.0
+   */
+  void stopCardPresenceMonitoringDuringProcessing();
+}
