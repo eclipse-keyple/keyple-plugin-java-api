2bd720238c3064997fb4c0f6bbfe44858444d6ce
Andrei Cristea <andrei.cristea019@gmail.com>
2024-03-27 16:30:13 +0100

    feat: restore processing state

--- src/main/java/org/eclipse/keyple/core/plugin/PluginApiProperties.java
+++ src/main/java/org/eclipse/keyple/core/plugin/PluginApiProperties.java
@@ -22,7 +22,7 @@ public final class PluginApiProperties {
    *
    * @since 2.0.0
    */
-  public static final String VERSION = "2.2";
+  public static final String VERSION = "2.3";
 
   /** Private constructor */
   private PluginApiProperties() {}
--- src/main/java/org/eclipse/keyple/core/plugin/spi/reader/observable/state/processing/package-info.java
+++ src/main/java/org/eclipse/keyple/core/plugin/spi/reader/observable/state/processing/package-info.java
@@ -1,10 +1,8 @@
 /**
  * SPI for the card processing observation profile definition.
  *
- * <p><b>Caution:</b> An observable reader must implement <b>one and only one</b> of these
- * interfaces.
+ * <p><b>Caution:</b> An observable reader can implement one of these interfaces if needed.
  *
  * @since 2.0.0
- * @deprecated Useless SPIs, don't use it. Will be removed in a future version of this API.
  */
 package org.eclipse.keyple.core.plugin.spi.reader.observable.state.processing;
